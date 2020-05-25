19c5748c816bc75ad7ba3b25f9c9443664c1aba2
Andres Canavesi <andres.canavesi@gmail.com>
2020-05-25 15:39:14 -0300

    deleted unused staff

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const log4js = require('log4js');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 
 const logger = log4js.getLogger('app.js');
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
