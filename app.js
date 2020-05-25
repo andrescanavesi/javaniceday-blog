0f1c8f313988045380c0f57b227dc5ff1c2329a9
Andres Canavesi <andres.canavesi@gmail.com>
2020-05-25 15:02:03 -0300

    added log4js

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const log4js = require('log4js');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const logger = log4js.getLogger('app.js');
 
 var app = express();
 
@@ -13,7 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -22,13 +23,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
+  logger.error(err);
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 const fs = require('fs');
 var path = require('path');
+const log4js = require('log4js');
 var router = express.Router();
+
+const logger = log4js.getLogger('routes/index.js');
+logger.level = 'info';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,25 +14,38 @@ router.get('/', function(req, res, next) {
 
 router.get('/post/:name', async function(req, res, next) {
   try{
-    const content =  path.resolve(__dirname,'../content/posts/'+req.params.name+'.html');
-    console.info('reading file', content);
-    const promise = new Promise((resolve, reject)=>{
-      fs.readFile(content, 'utf8', function (err,data) {
-        if (err) {
-          reject(new Error(err));
-        }else{
-          resolve(data);
-        }
-        
-      });
-    });
-    const data = await promise;
-    res.render('content', { title:'Im a page', content: data });
-   
+    const data = await readContent(req.params.name, 'posts');
+    res.render('content', { title:'Im a post', content: data });
   }catch(e){
     next(e);
   }
-  
 });
+
+router.get('/page/:name', async function(req, res, next) {
+  try{
+    const data = await readContent(req.params.name, 'pages');
+    res.render('content', { title:'Im a page', content: data });
+  }catch(e){
+    next(e);
+  }
+});
+
+function readContent(name, kind = 'posts'){
+  if(!name){
+    throw new Error('name param is empty');
+  }
+  const file = name+'.html';
+  const content =  path.resolve(__dirname,'../content/', kind,file);
+  logger.info('reading file:', content);
+  return new Promise((resolve, reject)=>{
+    fs.readFile(content, 'utf8', function (err,data) {
+      if (err) {
+        reject(new Error(err));
+      }else{
+        resolve(data);
+      }
+    });
+  });
+}
 
 module.exports = router;
