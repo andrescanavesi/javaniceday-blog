96f6c86ff42693d3c90941525e3d27e6aad3fd98
Andres Canavesi <andres.canavesi@gmail.com>
2021-05-07 22:42:05 -0300

    https redirect

--- README.md
+++ README.md
@@ -15,3 +15,18 @@ npm test
 ```
 
 Open `http://localhost:3000`
+
+# See logs in Heroku
+```bash
+heroku logs --tail -a javaniceday-blog-staging
+```
+
+# Set env vars in heroku
+```bash
+heroku config:set -a javaniceday-blog-staging NODE_ENV=staging 
+heroku config:get -a javaniceday-blog-staging NODE_ENV
+```
+
+# Set all env vars in Heroku from .env file (WARNING: it will override all!)
+`heroku config:set -a javaniceday-blog-staging $(cat .env | sed '/^$/d; /#[[:print:]]*$/d')`
+
--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const compression = require('compression');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const log4js = require('log4js');
+const utils = require('./utils/utils');
 
 const indexRouter = require('./routes/index');
 const adminRouter = require('./routes/admin');
@@ -44,6 +45,21 @@ function shouldCompress(req, res) {
 }
 
 const app = express();
+
+// redirect any page form http to https
+app.use((req, res, next) => {
+  logger.info(`*** host: ${req.headers.host}`);
+  logger.info(`*** url: ${req.url}`);
+  if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'test' && !utils.isSecure(req)) {
+    const host = req.headers.host.startsWith('www.') ? req.headers.host : `www.${req.headers.host}`;
+    const url = `https://${host}${req.url}`;
+    logger.info(`redirecting from http to https: ${url}`);
+    res.redirect(301, url);
+  } else {
+    next();
+  }
+});
+
 app.use(compression({ filter: shouldCompress }));
 app.use(useragent.express());
 
